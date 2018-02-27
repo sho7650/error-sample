c60d6e4bafa49832faba63d0159dd51b43acdbd7
sho kisaragi <sho@oshiire.to>
2018-02-27 12:03:25 +0900

    Added H12 error sample

--- routes/h12.js
+++ routes/h12.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  setTimeout(() => {
+    res.send('33秒経過');
+  }, 33000);
 });
 
 module.exports = router;
